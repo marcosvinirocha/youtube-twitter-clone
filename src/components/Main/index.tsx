556903ef7be1e74413411cc2d11b7be97823de0b
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2020-07-09 15:19:01 -0300

    Initial project

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import ProfilePage from '../ProfilePage';
+
+import {
+  Container,
+  Header,
+  BackIcon,
+  ProfileInfo,
+  BottomMenu,
+  HomeIcon,
+  SearchIcon,
+  BellIcon,
+  EmailIcon
+} from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+        <ProfileInfo>
+          <strong>Marcos Vinicius</strong>
+          <span>612 tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      <ProfilePage/>
+
+      <BottomMenu>
+        <HomeIcon />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu>
+    </Container>
+  );
+};
+
+export default Main;
---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocalitionIcon,
+  CakeIcon,
+  Followage,
+  EditButton
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        <EditButton outlined> Editar perfil</EditButton>
+        <h1>Marcos Vinicius</h1>
+        <h2>@marcosudia</h2>
+
+        <p>
+          Developer at <a href="https://github.com/marcosudia">@Developer</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocalitionIcon />
+            Uberlândia, Brazil
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <CakeIcon />
+            Nascido em 06 de maio de 1995
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            Seguindo <strong> 1000</strong>
+          </span>
+          <span>
+            <strong>100 </strong>Seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+    </Container>
+  );
+};
+
+export default ProfilePage;
